3da89868d9ac8c6c79d839f5b0f93cf9a8ae644c
Philip osir Otieno <philiposir2@gmail.com>
2024-06-25 12:06:27 +0300

    updates on class

---
+++ index.js
@@ -0,0 +1,21 @@
+
+    var userName = "philip";
+    var passWord = 12345;
+    console.log(userName, passWord)
+    var userName;
+    let isUserName = true;
+    let isPassword = false;
+    const myUsername = "John";
+    const myPassword = 88788;  
+    console.log(isUserName, isPassword)
+    console.log(myUsername, myPassword)
+    const secondName = "Philip"
+
+    let numberOne = Number("5");
+    let numberTwo = 5;
+    console.log(numberOne === numberTwo)
+
+    let numberThree = "5";
+    let numberFour = 5;
+    console.log(numberThree === numberFour)
+    
